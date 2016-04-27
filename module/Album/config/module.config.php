2292a7d6a713b9dec6aa2585113f3f1f2d343ecc
edson <edson>
2016-04-27 15:48:29 +0200

    Adding OK

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -8,36 +8,17 @@ return array(
     'router' => array(
         'routes' => array(
             'album' => array(
-                'type'    => 'Literal',
+                'type'    => 'segment',
                 'options' => array(
-                    // Change this to something specific to your module
-                    'route'    => '/album',
-                    'defaults' => array(
-                        // Change this value to reflect the namespace in which
-                        // the controllers for your module are found
-                        '__NAMESPACE__' => 'Album\Controller',
-                        'controller'    => 'Album',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    // This route is a sane default when developing a module;
-                    // as you solidify the routes for your module, however,
-                    // you may want to remove it and replace it with more
-                    // specific routes.
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
+                    'route'    => '/album[/:action][/:id]',
+                     'constraints' => array(
+                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                         'id'     => '[0-9]+',
+                     ),
+                     'defaults' => array(
+                         'controller' => 'Album\Controller\Album',
+                         'action'     => 'index',
+                     ),
                 ),
             ),
         ),
@@ -48,3 +29,4 @@ return array(
         ),
     ),
 );
+
