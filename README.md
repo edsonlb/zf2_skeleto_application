e6fde93ac2ec74158095844a425966da618ded8a
edson <edson>
2016-04-27 16:35:09 +0200

    Configuring routes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-ZendSkeletonApplication
+ZendSkeletonApplication - Edson Version
 =======================
 
 Introduction
--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -20,6 +20,11 @@ namespace Album\Model;
          $this->title  = (isset($data['title']))  ? $data['title']  : null;
      }
 
+     public function getArrayCopy()
+     {
+         return get_object_vars($this);
+     }
+     
      // Add content to these methods:
      public function setInputFilter(InputFilterInterface $inputFilter)
      {
