4b129e189e64874c0e5b1e94e3e6c6f1bb1fed3f
edson <edson>
2016-04-28 10:39:57 +0200

    Creating more configurations

--- module/Blog/src/Blog/Controller/ListController.php
+++ module/Blog/src/Blog/Controller/ListController.php
@@ -2,7 +2,17 @@
  namespace Blog\Controller;
 
  use Zend\Mvc\Controller\AbstractActionController;
+ use Blog\Service\PostServiceInterface;
 
  class ListController extends AbstractActionController
  {
+     /**
+      * @var \Blog\Service\PostServiceInterface
+      */
+     protected $postService;
+     
+     public function __construct(PostServiceInterface $postService)
+     {
+         $this->postService = $postService;
+     }
  }
